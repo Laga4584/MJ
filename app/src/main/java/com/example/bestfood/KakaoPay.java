12eca229e21e07d2c49558db0d0ea6beeac83603
MINJI-CS <minji000225@gmail.com>
2021-01-12 15:58:37 +0900

    .

--- app/src/main/java/com/example/bestfood/KakaoPay.java
+++ app/src/main/java/com/example/bestfood/KakaoPay.java
@@ -36,4 +36,32 @@ public class KakaoPay {
                 .build();
         Response response = client.newCall(request).execute();
     }
+
+    private void kakaoPayOrder() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded;charset=utf-8");
+        RequestBody body = RequestBody.create(mediaType, "cid=TC0ONETIME&tid=T2850892840739524675");
+        Request request = new Request.Builder()
+                .url("https://kapi.kakao.com/v1/payment/order")
+                .method("POST", body)
+                .addHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8")
+                .addHeader("Authorization", "KakaoAK ceb9ff5abc3653edac21733f89a1f68f")
+                .build();
+        Response response = client.newCall(request).execute();
+    }
+
+    private void kakaoPayCancel() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded;charset=utf-8");
+        RequestBody body = RequestBody.create(mediaType, "cid=TC0ONETIME&tid=T1234567890123456789&cancel_amount=2200&cancel_tax_free_amount=0&cancel_vat_amount=200&cancel_available_amount=4000");
+        Request request = new Request.Builder()
+                .url("https://kapi.kakao.com/v1/payment/cancel")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+                .addHeader("Authorization", "KakaoAK ceb9ff5abc3653edac21733f89a1f68f")
+                .build();
+        Response response = client.newCall(request).execute();
+    }
 }
